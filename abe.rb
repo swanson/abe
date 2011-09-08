4582eccae81a81cae7697d2b141ed36154091e5a
Matt Swanson <mdswanson@sep.com>
2011-09-07 23:58:08 -0400

    masterpiece completed

--- abe.rb
+++ abe.rb
@@ -1,14 +1,30 @@
 require 'sinatra'
 require 'erb'
 require 'json'
+require 'mongo'
 
 class Abe < Sinatra::Base
+
+  configure do
+    uri_string = uri_string = ENV['MONGOHQ_URL'] || ENV['MONGOHQ_ABE']
+    uri = URI.parse(uri_string)
+    conn = Mongo::Connection.from_uri(uri_string)
+    db = conn.db(uri.path.gsub(/^\//, ''))
+    $coll = db.collection('facts')
+  end
+
   get '/' do
+    r = rand(42)
+    fact = $coll.find_one({'random' => r})
+    @pres = fact['pres']
+    @fact = fact['fact']
     erb :index
   end
 
   get '/fact' do
     content_type :json
-    {"fact" => "got stuck in a bathtub?", "pres" => "Taft"}.to_json
+    r = rand(42)
+    fact = $coll.find_one({'random' => r})
+    fact.to_json
   end
 end
--- seed.rb
+++ seed.rb
@@ -8,15 +8,18 @@ uri = URI.parse(uri_string)
 conn = Mongo::Connection.from_uri(uri_string)
 db = conn.db(uri.path.gsub(/^\//, ''))
 facts = db.collection("facts")
+facts.remove
 
 file = File::open('president_list.txt', 'r')
 
+i = 0
 file.each do |line|
  cols = line.split('|')
  doc = {
     "pres" => cols[0],
-    "fact" => cols[1],
-    "random" => rand()
+    "fact" => cols[1].strip,
+    "random" => i
  }
+ i+=1
  facts.insert(doc)
 end
